6a51f90d3fe74b2a81f5c658b1293b92238d9db5
Martin Feussner <107300117+martinfeussner@users.noreply.github.com>
2025-08-04 19:39:48 +0200

    Delete DEFIv2-1 directory

--- DEFIv2-1/Reference Implementation/defiv2_keygen.h
+++
@@ -1,6 +0,0 @@
-#ifndef defiv2_keygen_h
-#define defiv2_keygen_h
-
-int key_gen(unsigned char *pk, unsigned char *sk);
-
-#endif
--- DEFIv2-1/Reference Implementation/defiv2_siggen.h
+++
@@ -1,6 +0,0 @@
-#ifndef defiv2_siggen_h
-#define defiv2_siggen_h
-
-int sig_gen(unsigned char *sm, unsigned long long *smlen, const unsigned char *m, unsigned long long mlen, const unsigned char *sk);
-
-#endif
